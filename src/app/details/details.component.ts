bf8fbdb06562dcbb15f01b7c167dc1a8c607667d
Grigoriy Shishmintsev <gvshishm@mts.ru>
2019-11-29 14:09:05 +0300

    rating

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { MoviesService } from '../shared/services/movies.service';
 import { Observable } from 'rxjs';
 import { Movie } from '../shared/models/movie.model';
 import { ActivatedRoute } from '@angular/router';
+import rater from 'rater-js';
 
 @Component({
   selector: 'app-details',
@@ -12,6 +13,19 @@ import { ActivatedRoute } from '@angular/router';
 export class DetailsComponent implements OnInit {
 
   public movie$: Observable<Movie>;
+
+  @ViewChild('rater', { static: false }) set content(elRef: ElementRef) {
+
+    const element = elRef.nativeElement;
+    const rating = element.getAttribute('rate');
+    const raterElement = rater({
+      element,
+      max: 10,
+      starSize: 24,
+      readOnly: true
+    });
+    raterElement.setRating(Number(rating));
+  }
 
   constructor(
     private moviesService: MoviesService,
