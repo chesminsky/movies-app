0507d82997740b05e07966031e01bf88f4991f7e
chesminsky <chesminsky@gmail.com>
2019-12-01 21:19:36 +0300

    list component tests

--- src/app/list/list.component.spec.ts
+++ src/app/list/list.component.spec.ts
@@ -1,25 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
+import { MoviesService } from '../shared/services/movies.service';
 
-xdescribe('ListComponent', () => {
-  let component: ListComponent;
-  let fixture: ComponentFixture<ListComponent>;
+describe('ListComponent', () => {
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ListComponent ]
-    })
-    .compileComponents();
-  }));
+  it('#clearFilter() should clear all filters', () => {
+    const comp = new ListComponent({} as MoviesService);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    comp.genres.setValue(['some random tag']);
+    comp.search.setValue('some name');
+
+    comp.clearFilter();
+
+    expect(comp.genres.value).toEqual([]);
+    expect(comp.search.value).toBe('');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('#searchByTag() should add selected genre to filter', () => {
+    const comp = new ListComponent({} as MoviesService);
+
+    comp.genres.setValue(['tag 1']);
+
+    comp.searchByTag('tag 2');
+
+    expect(comp.genres.value).toEqual(['tag 1', 'tag 2']);
   });
 });
